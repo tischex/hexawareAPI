40e1187af37d372044b0156bbd8f932a5b440489
tischex <107503976+tischex@users.noreply.github.com>
2022-06-15 11:51:06 -0400

    AI off
    Drop the @GeneratedValue

--- hexaware/src/main/java/com/api/hexaware/model/Library.java
+++ hexaware/src/main/java/com/api/hexaware/model/Library.java
@@ -2,8 +2,6 @@ package com.api.hexaware.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +10,6 @@ import javax.persistence.Table;
 public class Library {
 	@Id
 	@Column(name="library_id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long library_id;
 	
 	@Column(name="library_name")
