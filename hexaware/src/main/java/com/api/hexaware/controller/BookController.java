4c6d7876ab40109b362d4615381cd53683c0fe92
tischex <107503976+tischex@users.noreply.github.com>
2022-06-16 18:30:17 -0400

    Count books
    New method to count books by library

--- hexaware/src/main/java/com/api/hexaware/controller/BookController.java
+++ hexaware/src/main/java/com/api/hexaware/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,14 @@ public class BookController {
 	@GetMapping("/bookList")
 	public List<Book> getAllBooks() {
 		return bookService.getAllBooks();
+	}
+	
+	@GetMapping("/numOfBooks/{library_library_id}")
+	public String countBooks(@PathVariable("library_library_id") int library_library_id) throws Exception {
+		String numOfBooks = "0 books";
+		if(bookService.getCountLibraryByLibraryId(library_library_id) > 0) {
+			numOfBooks = "" + bookService.getCountLibraryByLibraryId(library_library_id) + " books";
+		}
+		return numOfBooks;
 	}
 }
