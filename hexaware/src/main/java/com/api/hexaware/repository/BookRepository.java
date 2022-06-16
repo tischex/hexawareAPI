13afdbd7edc612cc9845c523e29bd6edfb5a2f18
tischex <107503976+tischex@users.noreply.github.com>
2022-06-16 18:27:40 -0400

    Count books by library
    New method to count books by library

--- hexaware/src/main/java/com/api/hexaware/repository/BookRepository.java
+++ hexaware/src/main/java/com/api/hexaware/repository/BookRepository.java
@@ -1,10 +1,13 @@
 package com.api.hexaware.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.api.hexaware.model.Book;
 
 @Repository("bookRepository")
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+	@Query(value = "SELECT COUNT(*) FROM BOOK b WHERE b.library_library_id =?1", nativeQuery = true)
+	public int countByLibraryId(@Param("library_library_id") int library_library_id);
 }
